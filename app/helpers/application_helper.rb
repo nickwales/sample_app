672c13804bb5f1c5c6144b2893a35157d9e8413a
Nick Wales <nwales@nick.nickwales.co.uk>
2011-06-28 20:27:52 +0100

    user model with passwords, and user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+
+ def logo
+  base_logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+ end
+ 
+# Return a title on a per-page basis
+ def title
+  base_title = "This is the title"
+  if @title.nil?
+   base_title
+  else 
+   "#{base_title} | #{@title}"
+  end
+ end 
+
 end
