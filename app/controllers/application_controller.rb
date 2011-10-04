7c403343fc73cb5a30720559e385f2c56fd75f2c
Nick Wales <nick@nickwales.co.uk>
2011-10-04 12:55:23 +0100

    Never liked unless statements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,15 @@
       win1 = winner.to_i
       los1 = loser.to_i
       unless Ranking.where(:user_id => winner)
-        p1 = Elo::Player.new(:rating => Ranking.where(:user_id => winner).last.score)
-      else
         p1 = Elo::Player.new(:rating => 1000)
+      else
+        p1 = Elo::Player.new(:rating => Ranking.where(:user_id => winner).last.score)
       end
       unless Ranking.where(:user_id => loser)
-        p2 = Elo::Player.new(:rating => Ranking.where(:user_id => loser).last.score)
-      else
         p2 = Elo::Player.new(:rating => 1000)
+      else
+        p2 = Elo::Player.new(:rating => Ranking.where(:user_id => loser).last.score)
+
       end
       game = p1.versus(p2)
       game.winner = p1
